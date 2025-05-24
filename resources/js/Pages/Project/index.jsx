c372513eedef29c97a0a41d497799daf47672d13
Yurei <69757457+Yurei21@users.noreply.github.com>
2025-05-24 14:56:34 +0800

    added shit

---
+++ resources/js/Pages/Project/index.jsx
@@ -0,0 +1,9 @@
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+
+export default function Index({}) {
+    return (
+        <AuthenticatedLayout>
+            
+        </AuthenticatedLayout>
+    )
+}
