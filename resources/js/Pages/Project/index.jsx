8df4454fcf08b02854fb8837f123e63a06c6c97f
Yurei <69757457+Yurei21@users.noreply.github.com>
2025-05-30 11:52:58 +0800

    Sorting done

--- resources/js/Pages/Project/index.jsx
+++ resources/js/Pages/Project/index.jsx
@@ -23,6 +23,21 @@ export default function Index({projects, queryParams = null}) {
         searchFieldChanged(name, e.target.value);
     }
 
+    const sortChanged = (name) => {
+        if (name === queryParams.sort_field) {
+            if (queryParams.sort_direction === "asc") {
+                queryParams.sort_direction = "desc"
+            } else {
+                queryParams.sort_direction = "asc"
+            }
+        } else {
+            queryParams.sort_field = name;
+            queryParams.sort_direction = 'asc'
+        }
+
+        router.get(route('project.index'), queryParams)
+    }
+
     return (
         <AuthenticatedLayout
             header={
@@ -37,74 +52,75 @@ export default function Index({projects, queryParams = null}) {
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
                         <div className="p-6 text-gray-900 dark:text-gray-100">
-                            
-                            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
-                                        <th className="px-3 py-2">ID</th>
-                                        <th className="px-3 py-2">Image</th>
-                                        <th className="px-3 py-2">Name</th>
-                                        <th className="px-3 py-2">Status</th>
-                                        <th className="px-3 py-2">Created Date</th>
-                                        <th className="px-3 py-2">Due Date</th>
-                                        <th className="px-3 py-2">Created By</th>
-                                        <th className="px-3 py-2 text-right">Action</th>
-                                </thead>
-                                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
-                                    <tr className="text-nowrap">
-                                        <th className="px-3 py-2"></th>
-                                        <th className="px-3 py-2"></th>
-                                        <th className="px-3 py-2">
-                                            <TextInput
-                                            className="w-full"
-                                            defaultValue={queryParams.name}
-                                            placeholder="Project Name"
-                                            onBlur={e => searchFieldChanged('name', e.target.value)}
-                                            onKeyPress={e => onKeyPress('name', e)}
-                                            />
-                                        </th>
-                                        <th className="px-3 py-2">
-                                            <SelectInput 
-                                            defaultValue={queryParams.status}
-                                            onChange={e => searchFieldChanged('status', e.target.value)}
-                                            className="w-full">
-                                                <option value="">Select Status</option>
-                                                <option value="pending">Pending</option>
-                                                <option value="in_progress">In Progress</option>
-                                                <option value="completed">Completed</option>
-                                            </SelectInput>
-                                        </th>
-                                        <th className="px-3 py-2"></th>
-                                        <th className="px-3 py-2"></th>
-                                        <th className="px-3 py-2"></th>
-                                        <th className="px-3 py-2 "></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {projects.data.map(project => (
-                                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={project.id}>
-                                            <td className="px-3 py-2">{project.id}</td>
-                                            <td className="px-3 py-2"><image src={project.image_path} alt="" style={{width:60}}/></td>
-                                            <td className="px-3 py-2">{project.name}</td>
-                                            <td className="px-3 py-2">
-                                                <span className={"px-2 py-1 rounded text-white " + PROJECT_STATUS_CLASS_MAP[project.status]}>
-                                                    {PROJECT_STATUS_TEXT_MAP[project.status]}
-                                                </span>
-                                            </td>
-                                            <td className="px-3 py-2">{project.created_at}</td>
-                                            <td className="px-3 py-2 text-nowrap">{project.due_date}</td>
-                                            <td className="px-3 py-2">{project.createdBy.name}</td>
-                                            <td className="px-3 py-2">
-                                                <Link href={route('project.edit', project.id)} className="font-medium text-blue-600 dark:text-blue-500 hover:underline mx-1">
-                                                    Edit
-                                                </Link>
-                                                <Link href={route('project.destroy', project.id)} className="font-medium text-red-600 dark:text-red-500 hover:underline mx-1">
-                                                    Delete
-                                                </Link>
-                                            </td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
+                                <div className="overflow-auto">
+                                    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
+                                                <th onClick={e => sortChanged('id')} className="px-3 py-2">ID</th>
+                                                <th onClick={e => sortChanged('')} className="px-3 py-2">Image</th>
+                                                <th onClick={e => sortChanged('name')} className="px-3 py-2">Name</th>
+                                                <th onClick={e => sortChanged('status')} className="px-3 py-2">Status</th>
+                                                <th onClick={e => sortChanged('created_at')} className="px-3 py-2">Created Date</th>
+                                                <th onClick={e => sortChanged('due_date')} className="px-3 py-2">Due Date</th>
+                                                <th onClick={e => sortChanged('')} className="px-3 py-2">Created By</th>
+                                                <th onClick={e => sortChanged('')} className="px-3 py-2 text-right">Action</th>
+                                        </thead>
+                                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
+                                            <tr className="text-nowrap">
+                                                <th className="px-3 py-2"></th>
+                                                <th className="px-3 py-2"></th>
+                                                <th className="px-3 py-2">
+                                                    <TextInput
+                                                    className="w-full"
+                                                    defaultValue={queryParams.name}
+                                                    placeholder="Project Name"
+                                                    onBlur={e => searchFieldChanged('name', e.target.value)}
+                                                    onKeyPress={e => onKeyPress('name', e)}
+                                                    />
+                                                </th>
+                                                <th className="px-3 py-2">
+                                                    <SelectInput 
+                                                    defaultValue={queryParams.status}
+                                                    onChange={e => searchFieldChanged('status', e.target.value)}
+                                                    className="w-full">
+                                                        <option value="">Select Status</option>
+                                                        <option value="pending">Pending</option>
+                                                        <option value="in_progress">In Progress</option>
+                                                        <option value="completed">Completed</option>
+                                                    </SelectInput>
+                                                </th>
+                                                <th className="px-3 py-2"></th>
+                                                <th className="px-3 py-2"></th>
+                                                <th className="px-3 py-2"></th>
+                                                <th className="px-3 py-2 "></th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {projects.data.map(project => (
+                                                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={project.id}>
+                                                    <td className="px-3 py-2">{project.id}</td>
+                                                    <td className="px-3 py-2"><image src={project.image_path} alt="" style={{width:60}}/></td>
+                                                    <td className="px-3 py-2">{project.name}</td>
+                                                    <td className="px-3 py-2">
+                                                        <span className={"px-2 py-1 rounded text-white " + PROJECT_STATUS_CLASS_MAP[project.status]}>
+                                                            {PROJECT_STATUS_TEXT_MAP[project.status]}
+                                                        </span>
+                                                    </td>
+                                                    <td className="px-3 py-2">{project.created_at}</td>
+                                                    <td className="px-3 py-2 text-nowrap">{project.due_date}</td>
+                                                    <td className="px-3 py-2">{project.createdBy.name}</td>
+                                                    <td className="px-3 py-2">
+                                                        <Link href={route('project.edit', project.id)} className="font-medium text-blue-600 dark:text-blue-500 hover:underline mx-1">
+                                                            Edit
+                                                        </Link>
+                                                        <Link href={route('project.destroy', project.id)} className="font-medium text-red-600 dark:text-red-500 hover:underline mx-1">
+                                                            Delete
+                                                        </Link>
+                                                    </td>
+                                                </tr>
+                                            ))}
+                                        </tbody>
+                                    </table>
+                                </div>
                             <Pagination links={projects.meta.links}></Pagination>
                         </div>
                     </div>
